ff8c37cf3cddf10af25991ef4679e7db27f65bda
Ayush Nainwal <iayush.n2@gmail.com>
2023-08-02 15:24:38 +0530

    Aliases for BrowserRouter removed

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import "./index.css"
 import Home from './pages/Home';
 import {Team} from "./pages/Team"
-import {BrowserRouter as Router,Link,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Link,Routes,Route} from "react-router-dom"
 
 
 const App = () => {
   return (
     
     <div className='App'>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/teams" element={<Team/>}/>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
     
      
