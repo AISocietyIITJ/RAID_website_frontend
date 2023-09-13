22c075c80d0167e90a3dd7a2d6cb1243107b5de6
adityarathor007 <adityarathor120@gmail.com>
2023-09-13 23:26:20 +0530

    removed eslint error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./index.css"
-<<<<<<< HEAD
-import Home from "./pages/Home";
-import {Team} from "./pages/Team";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-=======
 import Home from './pages/Home';
 import {Team} from "./pages/Team"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import {Achievements} from "./pages/Achievements"
 import {Projects} from "./pages/Projects"
 import Navbar from "./components/navbar/Navbar"
->>>>>>> 55fda1d145520345b15e289163b3280f25be7a25
 
 
 const App = () => {
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,18 +1,4 @@
 import React from "react"
-<<<<<<< HEAD:src/pages/Team.js
-import Navbar from "../Navbar"
-import "../styles/team.css"
-
-export const Team=()=>{
-  return (
-    <div>
-      <Navbar />
-      <div className="team_page_content">
-        {/* Enter the content for team page here*/ }
-        <h1>This is the team page</h1>
-      </div>
-    </div>
-=======
 
 
 import "../styles/team.css"
@@ -28,7 +14,6 @@ export const Team=()=>{
                
             </div>
         </div>
->>>>>>> 55fda1d145520345b15e289163b3280f25be7a25:src/pages/Team.jsx
         
   )
 }
